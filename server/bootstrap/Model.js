eacd1e69cc7655e52068075d0f73f8a8633a1b3e
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2020-05-29 05:19:48 +0530

    chore: model modified according to sequalize

--- server/bootstrap/Model.js
+++ server/bootstrap/Model.js
@@ -1,22 +1,90 @@
 const fs = require("fs");
-const Path = require("path");
+const path = require("path");
+const Sequelize = require("sequelize");
 
-const init = async () => {
-    const schemas = fs.readdirSync(Path.resolve(__dirname, "../models"));
-    const resolvedSchemas = await schemas.reduce(async (acc, file) => {
-        try {
-            const res = await require(`./../models/${file}`);
-            const dep = file.split(".")[0];
-            acc[dep] = await Mongoose.model(dep, res);
-            global[dep] = acc[dep];
-            Print.info(`Dependency [${file}] loaded from [models]`);
-            return acc;
-        } catch (e) {
-            Print.error(`Unable to load ${file}`, e);
-            return e;
-        }
-    }, {});
-    return resolvedSchemas;
+const attributeMapping = {
+    required: "allowNull"
 };
 
-module.exports = init;
+function capitalize(string="") {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+class ModelBuilder {
+    constructor(dir) {
+        this.dir = dir;
+        this.files = fs.readdirSync(dir);
+        this.loadModels = this.loadModels.bind(this);
+        this.getModel = this.getModel.bind(this);
+        this.loadModels();
+    }
+    static DataTypes = Sequelize;
+
+    static convertColumnAttributes(attributes) {
+        return Object.keys(attributes).reduce((acc, key) => {
+            if (key in attributeMapping) {
+                acc[attributeMapping[key]] = attributes[key];
+            } else if (typeof attributes[key] === "object" && !attributes[key].length) {
+                acc[key] = ModelBuilder.convertColumnAttributes(attributes[key]);
+            } else {
+                acc[key] = attributes[key];
+            }
+            return acc;
+        }, {});
+    }
+
+    static convertDataTypes(attributes) {
+        return Object.keys(attributes).reduce((acc, key) => {
+            const column = attributes[key];
+            if (typeof column === "string") {
+                acc[key] = ModelBuilder.DataTypes[column.toUpperCase()];
+            } else {
+                acc[key] = {
+                    ...column,
+                    type: ModelBuilder.DataTypes[column.type ? column.type.toUpperCase() : "STRING"]
+                };
+            }
+            return acc;
+        }, {});
+    }
+
+    static sanitizeFields(columnFields) {
+        try {
+            const updatedAttributes = ModelBuilder.convertColumnAttributes(columnFields);
+            return ModelBuilder.convertDataTypes(updatedAttributes);
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    async getModel(file) {
+        try {
+            const modelDef = await require(`${this.dir}/${file}`);
+            const { attributes, modelName, ...options } = modelDef;
+            const tableName = modelName || file.split(".")[0];
+            const sanitizedSchema = ModelBuilder.sanitizeFields(attributes);
+            global[capitalize(tableName)] = Db.define(tableName, sanitizedSchema, options);
+            return global[capitalize(tableName)];
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    async loadModels() {
+        const resolvedSchemas = await this.files.reduce(async (acc, file) => {
+            try {
+                await this.getModel(file);
+                Print.info(`Dependency [${file}] loaded from [models]`);
+                return acc;
+            } catch (e) {
+                Print.error(`Unable to load ${file}`);
+                throw Error(e);
+                return e;
+            }
+        }, {});
+        return resolvedSchemas;
+    }
+}
+
+
+module.exports = new ModelBuilder(path.resolve(__dirname, "../models-2"));
